1bc4ef012c69182934cda60e70b311724c8f36c4
Aneesh833 <kadala2@rpi.edu>
2021-10-14 01:19:44 -0400

    Explore bar added
    may need to dynamically add flip cards (not sure yet) and need to see if I can filter based on interests yet. Added some general css/formatting take a look

--- pages/main.js
+++ pages/main.js
@@ -3,18 +3,12 @@ function changeStyle(){
     element.style.display = "none";
 }
 function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
+    
+    var input = document.getElementById("search").value;
+
+    if (input && input.length>0){
+        alert("hello")
+        $('.flex-container_2').find('.flip-card.flip-card-inner.flip-card-front.h3:not(:contains("'+input+'"))').parentsUntil('.flip-card').parent().addClass('d-none');
+        alert("hello world")
+    };
+};
