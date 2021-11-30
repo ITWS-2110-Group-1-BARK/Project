cfb74baeb5787e94e917d72c0b971d4042073eae
ruchika-singh <ruchika30.singh@gmail.com>
2021-11-30 11:09:05 -0500

    redirect to login

--- pages/signup.php
+++ pages/signup.php
@@ -22,8 +22,8 @@ $nameErr ="";
 		<img id="logo" src="../logo.png" alt= "LOGO">
 		<a class = "active" href="#">Sign Up</a>
 		<a  href="login.php">Login</a>
-		<a href="profile.html">Profile</a>
-		<a href="explore.html">Explore</a>
+		<a href="profile.php">Profile</a>
+		<a href="explore.php">Explore</a>
         <a href="../homepage.html">Homepage</a>
 	</div>
     
