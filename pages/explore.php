2bace01ca00b187342ac03a5d79af82dfbdba990
Aneesh833 <kadala2@rpi.edu>
2021-12-09 17:03:00 -0500

    fixed javascript for explore page

--- pages/explore.php
+++ pages/explore.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../style.css"/>
-    <script type="text/javascript" src="explore_final.js"></script>
+    <script type="text/javascript" src="explore_final1.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body onFocus="parent_disable();" onclick="parent_disable();">
